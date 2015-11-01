7eb94ba111f801f25101f1d1051025eac2373cfe
cew <cewillismail@gmail.com>
2015-11-01 17:54:05 -0500

    applied gofmt -s

--- server.go
+++ server.go
@@ -9,28 +9,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/cewillis001/tftp"
 	"net"
 	"os"
-	"flag"
-	"github.com/cewillis001/tftp"
 )
 
 func CheckError(err error) {
-    if err  != nil {
-        fmt.Println("Error: " , err)
-        os.Exit(0)
-    }
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
 }
 
-func DeleteChannel(done <-chan *net.UDPAddr, ch map[string]chan []byte, verbose *bool){
-	for r := range done{
+func DeleteChannel(done <-chan *net.UDPAddr, ch map[string]chan []byte, verbose *bool) {
+	for r := range done {
 
-		if (*verbose) {
-			_,present := ch[r.String()]
-			if(present) { 
-				fmt.Println(r, " channel in map and scheduled for deletion") 
-			}else{
+		if *verbose {
+			_, present := ch[r.String()]
+			if present {
+				fmt.Println(r, " channel in map and scheduled for deletion")
+			} else {
 				fmt.Println(r, " channel not in map, something is off")
 			}
 		}
@@ -38,11 +38,11 @@ func DeleteChannel(done <-chan *net.UDPAddr, ch map[string]chan []byte, verbose 
 		close(ch[r.String()])
 		delete(ch, r.String())
 
-		if (*verbose) {
-			_,present := ch[r.String()]
-			if(present) {
+		if *verbose {
+			_, present := ch[r.String()]
+			if present {
 				fmt.Println(r, " was not deleted")
-			}else{
+			} else {
 				fmt.Println(r, " deleted")
 			}
 		}
@@ -50,25 +50,29 @@ func DeleteChannel(done <-chan *net.UDPAddr, ch map[string]chan []byte, verbose 
 	}
 }
 
-func WriteFile(in <-chan *tftp.File, m map[string][]byte, verbose *bool){
-  for r := range in {
-		if(*verbose) { fmt.Println(r.Name, " to be written ") }
-    _,present := m[r.Name]
-    if(!present) {
-      m[r.Name] = r.Data
-    }else{
-      errCode := []byte {0, 6}
-      errMsg  := "No overwriting allowed"
-			if(*verbose) { fmt.Println(errMsg) }
-      tftp.SendERRORTo(errCode, errMsg, r.Conn, r.Raddr)
-    }
-  }
+func WriteFile(in <-chan *tftp.File, m map[string][]byte, verbose *bool) {
+	for r := range in {
+		if *verbose {
+			fmt.Println(r.Name, " to be written ")
+		}
+		_, present := m[r.Name]
+		if !present {
+			m[r.Name] = r.Data
+		} else {
+			errCode := []byte{0, 6}
+			errMsg := "No overwriting allowed"
+			if *verbose {
+				fmt.Println(errMsg)
+			}
+			tftp.SendERRORTo(errCode, errMsg, r.Conn, r.Raddr)
+		}
+	}
 }
 
 func main() {
 	/* flag for turning on verbose output */
 	verbose := flag.Bool("verbose", false, "a bool")
-  flag.Parse()
+	flag.Parse()
 
 	/* local memory for storing files */
 	m := map[string][]byte{} //creates map of empty files
@@ -82,115 +86,139 @@ func main() {
 	go DeleteChannel(done, ch, verbose)
 
 	/* channel for the one writer go routine */
- 	newFile := make(chan *tftp.File)
+	newFile := make(chan *tftp.File)
 	defer close(newFile)
-	go WriteFile(newFile, m, verbose) 
+	go WriteFile(newFile, m, verbose)
 
-	/* Lets prepare a address at any address at port 10001*/   
-	ServerAddr,err := net.ResolveUDPAddr("udp",":10001")
+	/* Lets prepare a address at any address at port 10001*/
+	ServerAddr, err := net.ResolveUDPAddr("udp", ":10001")
 	CheckError(err)
- 
+
 	/* Now listen at selected port */
 	ServerConn, err := net.ListenUDP("udp", ServerAddr)
 	CheckError(err)
 	defer ServerConn.Close()
 
 	buf := make([]byte, 1024)
- 
+
 	for {
-		n,addr,err := ServerConn.ReadFromUDP(buf)
+		n, addr, err := ServerConn.ReadFromUDP(buf)
 		CheckError(err)
 
-		if(n < 2 || buf[0] != 0 || buf[1] == 0 || buf[1] > 5){
+		if n < 2 || buf[0] != 0 || buf[1] == 0 || buf[1] > 5 {
 			tftp.SendERRORTo([]byte{0, 0}, "bad opcode", ServerConn, addr)
 			continue
 		}
 
-		switch buf[1]{
-			case 1:
-				//handle RRQ
-				if(*verbose) {fmt.Println("Recived ", string(buf[0:n]), " from ", addr, " as RRQ")}
-				filename, err := tftp.GetRRQname(buf[0:n])
-				if(err != nil){
-					errCode := []byte {0, 0}
-					errMsg  := "Badly formed RRQ packet"
-					if(*verbose) {fmt.Println(errMsg, " from ", addr)}
-					tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
+		switch buf[1] {
+		case 1:
+			//handle RRQ
+			if *verbose {
+				fmt.Println("Recived ", string(buf[0:n]), " from ", addr, " as RRQ")
+			}
+			filename, err := tftp.GetRRQname(buf[0:n])
+			if err != nil {
+				errCode := []byte{0, 0}
+				errMsg := "Badly formed RRQ packet"
+				if *verbose {
+					fmt.Println(errMsg, " from ", addr)
+				}
+				tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
+			} else {
+				_, present := m[filename]
+				if present {
+					ch[addr.String()] = make(chan []byte)
+					go tftp.HandleRRQ(filename, ch[addr.String()], done, ServerConn, addr, m[filename], verbose)
 				} else {
-					_,present := m[filename]
-					if(present){
-						ch[addr.String()] = make(chan []byte) 
-						go tftp.HandleRRQ(filename, ch[addr.String()], done, ServerConn, addr, m[filename], verbose)
-					} else {
-						errCode := []byte {0, 1}
-						errMsg  := filename + " not found"
-						tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
+					errCode := []byte{0, 1}
+					errMsg := filename + " not found"
+					tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
+				}
+			}
+
+		case 2:
+			//handle WRQ
+			if *verbose {
+				fmt.Println("Recieved ", string(buf[0:n]), " from ", addr, " as WRQ")
+			}
+			filename, err := tftp.GetWRQname(buf[0:n])
+			if err != nil {
+				errCode := []byte{0, 0}
+				errMsg := "Badly formed WRQ packet"
+				if *verbose {
+					fmt.Println(errMsg, " from ", addr)
+				}
+				tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
+			} else {
+				_, present := m[filename]
+				if present {
+					errCode := []byte{0, 6}
+					errMsg := "No overwriting allowed"
+					if *verbose {
+						fmt.Println(errMsg)
 					}
-				}
-
-			case 2:
-				//handle WRQ
-				if(*verbose) {fmt.Println("Recieved ", string(buf[0:n]), " from ", addr, " as WRQ")}
-				filename, err := tftp.GetWRQname(buf[0:n])
-				if(err != nil){
-					errCode := []byte {0, 0}
-					errMsg  := "Badly formed WRQ packet"
-					if(*verbose) {fmt.Println(errMsg, " from ", addr)}
 					tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
 				} else {
-					_,present := m[filename]
-					if(present){
-						errCode := []byte {0, 6}
-						errMsg  := "No overwriting allowed"
-						if(*verbose) { fmt.Println(errMsg) }
-						tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
-					}else{
-						ch[addr.String()] = make(chan []byte)
-						_,present := ch[addr.String()]
-						if(present && *verbose){
-							fmt.Println(addr, " added successfully to map")
-						}
-						go tftp.HandleWRQ(filename, ch[addr.String()], done, ServerConn, addr, newFile, verbose)
-					}	
+					ch[addr.String()] = make(chan []byte)
+					_, present := ch[addr.String()]
+					if present && *verbose {
+						fmt.Println(addr, " added successfully to map")
+					}
+					go tftp.HandleWRQ(filename, ch[addr.String()], done, ServerConn, addr, newFile, verbose)
 				}
+			}
 
-			case 3:
-				//handle DATA
-				if (*verbose) { fmt.Println("  Recieved ", string(buf[0:n]), " from ", addr, " as DATA")}
-				_,present := ch[addr.String()]
-				if(present){
-					if(*verbose) {fmt.Println("  ", addr, " channel is present")}
-					ch[addr.String()] <- buf[0:n]
-				} else {
-					errCode := []byte {0, 5}
-					errMsg  := "No write request associated with this return address"
-					if(*verbose) {fmt.Println("  ", errMsg, " ", addr)}
-					tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
-				} 
-
-			case 4:
-				//handle ACK
-				if(*verbose) {fmt.Println("    Recieved ", string(buf[0:n]), " from ", addr, " as ACK")}
-				_,present := ch[addr.String()]
-				if(present){
-					if(*verbose) {fmt.Println("    ", addr, " channel is present")}
-					ch[addr.String()] <- buf[0:n]
-				} else {
-					errCode := []byte {0, 5}
-					errMsg  := "No read request associated with this return address"
-					if(*verbose) {fmt.Println("    ", errMsg, " ", addr) }
-					tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
+		case 3:
+			//handle DATA
+			if *verbose {
+				fmt.Println("  Recieved ", string(buf[0:n]), " from ", addr, " as DATA")
+			}
+			_, present := ch[addr.String()]
+			if present {
+				if *verbose {
+					fmt.Println("  ", addr, " channel is present")
 				}
+				ch[addr.String()] <- buf[0:n]
+			} else {
+				errCode := []byte{0, 5}
+				errMsg := "No write request associated with this return address"
+				if *verbose {
+					fmt.Println("  ", errMsg, " ", addr)
+				}
+				tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
+			}
 
-			case 5:
-				//handle ERROR
-				/* 
-					all errors should term connection, EXCEPT if the error is because
-					"the source port of a recieved tftp is incorrect. in this case
-					an errr tftp is sent to the originating host"
-				*/
-				if(*verbose){fmt.Println("      Recieved ", string(buf[0:n]), " from ", addr, " as ERROR")}
-				done <- addr
+		case 4:
+			//handle ACK
+			if *verbose {
+				fmt.Println("    Recieved ", string(buf[0:n]), " from ", addr, " as ACK")
+			}
+			_, present := ch[addr.String()]
+			if present {
+				if *verbose {
+					fmt.Println("    ", addr, " channel is present")
+				}
+				ch[addr.String()] <- buf[0:n]
+			} else {
+				errCode := []byte{0, 5}
+				errMsg := "No read request associated with this return address"
+				if *verbose {
+					fmt.Println("    ", errMsg, " ", addr)
+				}
+				tftp.SendERRORTo(errCode, errMsg, ServerConn, addr)
+			}
+
+		case 5:
+			//handle ERROR
+			/*
+				all errors should term connection, EXCEPT if the error is because
+				"the source port of a recieved tftp is incorrect. in this case
+				an errr tftp is sent to the originating host"
+			*/
+			if *verbose {
+				fmt.Println("      Recieved ", string(buf[0:n]), " from ", addr, " as ERROR")
+			}
+			done <- addr
 		}
 	}
 }
--- server_test.go
+++ server_test.go
@@ -1,31 +1,33 @@
 package main
- 
+
 import (
-    "fmt"
-    "net"
-    "time"
-		"testing"
-		"errors"
-    "strconv"
-		"github.com/cewillis001/tftp"
+	"errors"
+	"fmt"
+	"github.com/cewillis001/tftp"
+	"net"
+	"strconv"
+	"testing"
+	"time"
 )
 
 //a six hundred char "Lorem ipsum" string
 
-var longFile string = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu," 
- 
+var longFile string = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu,"
+
 func checkError(err error) {
-    if err  != nil {
-        fmt.Println("Error: " , err)
-    }
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
- 
+
 func min(a int, b int) int {
-	if(a <= b) {return a}
+	if a <= b {
+		return a
+	}
 	return b
 }
 
-func sendFullWrite(filename string, content string){
+func sendFullWrite(filename string, content string) {
 	//sends inital WRQ
 	//waits for ack block 0,0 or times out
 	//loop
@@ -33,7 +35,7 @@ func sendFullWrite(filename string, content string){
 	//  waits for ack, or handles error, or resends
 	//  if data sent and acknowledged was last data, end
 
-	ServerAddr,err := net.ResolveUDPAddr("udp","127.0.0.1:10001")
+	ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:10001")
 	checkError(err)
 
 	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
@@ -41,171 +43,180 @@ func sendFullWrite(filename string, content string){
 
 	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
 	checkError(err)
-	defer Conn.Close()	
+	defer Conn.Close()
 
 	/* initialize variables for tracking acknowledgements and location in content*/
 	index := 0
 	f := []byte(content)
 	max := min(512, len(f))
 	data := f[index:max]
-	block := []byte{0,0}
+	block := []byte{0, 0}
 
 	/*send inital WRQ*/
 
 	/*send inital WRQ*/
 	tftp.SendWRQ(filename, Conn)
-  packet := make(chan []byte)
+	packet := make(chan []byte)
 	defer close(packet)
 	go getPacket(Conn, packet)
 
 	select {
-		case <- time.After(15 * time.Second):
-				fmt.Println("WRQ from ", LocalAddr, " never acknowledged")
+	case <-time.After(15 * time.Second):
+		fmt.Println("WRQ from ", LocalAddr, " never acknowledged")
+		return
+	case r := <-packet:
+		switch r[1] {
+		case 4:
+			//ACK packet recieved
+			ackBlock, err := tftp.GetAckBlock(r)
+			if err != nil {
+				errCode := []byte{0, 0}
+				errMsg := "badly formed ACK packet"
+				tftp.SendERROR(errCode, errMsg, Conn)
 				return
-		case r := <- packet:
-				switch r[1]{
-					case 4:
-						//ACK packet recieved
-						ackBlock, err := tftp.GetAckBlock(r)
-						if(err != nil){
-							errCode := []byte{0,0}
-							errMsg  := "badly formed ACK packet"
-							tftp.SendERROR(errCode, errMsg, Conn)
-							return
-						}
-						if(ackBlock[1] == block[1]) { break }
-					
-					case 5:
-						//ERROR packet recieved
-						_, errMsg, err := tftp.GetError(r)
-						if(err != nil){ return }
-						fmt.Println(string(errMsg), " recieved by ", LocalAddr)
-						return			
-				}
+			}
+			if ackBlock[1] == block[1] {
+				break
+			}
+
+		case 5:
+			//ERROR packet recieved
+			_, errMsg, err := tftp.GetError(r)
+			if err != nil {
+				return
+			}
+			fmt.Println(string(errMsg), " recieved by ", LocalAddr)
+			return
+		}
 	}
 
-	SendLoop:
-	for{
+SendLoop:
+	for {
 		//send Data
 		block[1]++
 		tftp.SendDATA(block, data, Conn)
 
 		//wait for ack
-		LoopAck:
-		for{
+	LoopAck:
+		for {
 			select {
-				case <- time.After(time.Second * 3):
-					tftp.SendDATA(block, data, Conn)
-				case <- time.After(time.Second * 15):
-					fmt.Println("DATA packet ", string(block), " from ", LocalAddr, " never acknowledged")
-					return
-				case r := <- packet :
-					switch r[1]{
-						case 4:
-							//ACK packet recieved
-							ackBlock, err := tftp.GetAckBlock(r)
-							if(err != nil){
-								errCode := []byte{0,0}
-								errMsg  := "badly formed ACK packet"
-								tftp.SendERROR(errCode, errMsg, Conn)
-								return
-							}
-							if(ackBlock[1] == block[1]) { break LoopAck }
-
-						case 5:
-							//ERROR packet recieved
-							_, errMsg, err := tftp.GetError(r)
-							if(err != nil){
-								return
-							}
-							fmt.Println(string(errMsg), " recieved by ", LocalAddr)
-							return
+			case <-time.After(time.Second * 3):
+				tftp.SendDATA(block, data, Conn)
+			case <-time.After(time.Second * 15):
+				fmt.Println("DATA packet ", string(block), " from ", LocalAddr, " never acknowledged")
+				return
+			case r := <-packet:
+				switch r[1] {
+				case 4:
+					//ACK packet recieved
+					ackBlock, err := tftp.GetAckBlock(r)
+					if err != nil {
+						errCode := []byte{0, 0}
+						errMsg := "badly formed ACK packet"
+						tftp.SendERROR(errCode, errMsg, Conn)
+						return
 					}
+					if ackBlock[1] == block[1] {
+						break LoopAck
+					}
+
+				case 5:
+					//ERROR packet recieved
+					_, errMsg, err := tftp.GetError(r)
+					if err != nil {
+						return
+					}
+					fmt.Println(string(errMsg), " recieved by ", LocalAddr)
+					return
+				}
 			}
 		}
 
-
-		if(max == len(content)){
+		if max == len(content) {
 			break SendLoop
 		}
 
 		//update data
 		index += 512
-		max = min(index + 512, len(f))
-		data = f[index : max]
+		max = min(index+512, len(f))
+		data = f[index:max]
 
 	}
-	
+
 }
 
-func getPacket(Conn *net.UDPConn, out chan<- []byte){
-	for{
+func getPacket(Conn *net.UDPConn, out chan<- []byte) {
+	for {
 		buf := make([]byte, 1024)
-		n,_,err := Conn.ReadFromUDP(buf)
-		if(err == nil){
+		n, _, err := Conn.ReadFromUDP(buf)
+		if err == nil {
 			out <- buf[0:n]
 		} else {
 			return
 		}
-	}	
+	}
 }
 
-func sendFullRead(filename string) (string, error){
-		//sends RRQ
-		//loop:
-		//  listens for DATA with block ahead of prev ACK packet
-		//  send ACK packet for current DATA
-		//  if len(current DATA) < 512, send final ACK and close
-	ServerAddr,err := net.ResolveUDPAddr("udp","127.0.0.1:10001")
-  checkError(err)
+func sendFullRead(filename string) (string, error) {
+	//sends RRQ
+	//loop:
+	//  listens for DATA with block ahead of prev ACK packet
+	//  send ACK packet for current DATA
+	//  if len(current DATA) < 512, send final ACK and close
+	ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:10001")
+	checkError(err)
 
-  LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
-  checkError(err)
+	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	checkError(err)
 
-  Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
-  checkError(err)
-  defer Conn.Close()
+	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	checkError(err)
+	defer Conn.Close()
 
 	temp := []byte{}
-  tftp.SendRRQ(filename, Conn)
+	tftp.SendRRQ(filename, Conn)
 	prev_block := []byte{0, 0}
 	packet := make(chan []byte)
 	defer close(packet)
 	go getPacket(Conn, packet)
 
-	Read:
-	for{
+Read:
+	for {
 		select {
-			case r := <- packet:
-				switch r[1]{
-					case 3:
-						//DATA packet recieved
-						block, data, err := tftp.GetData(r)
-						if(err != nil){ return "", err }
-						if(block[1] == prev_block[1] + 1){
-							tftp.SendACK(block, Conn)
-							prev_block[1] = block[1]
-							temp = append(temp, data...)
-							if(len(data) < 512) { break Read }
-						}else if( block[1] != 0 ){
-							tftp.SendACK(prev_block, Conn)
-						} // else the RRQ itself failed somehow
-				
-					case 5:
-						//ERROR packet recieved
-            _, errMsg, err := tftp.GetError(r)
-            if(err != nil){
-              return "", err
-            }
-            fmt.Println(string(errMsg), " recieved by ", LocalAddr)
-            return "", errors.New(string(errMsg))
+		case r := <-packet:
+			switch r[1] {
+			case 3:
+				//DATA packet recieved
+				block, data, err := tftp.GetData(r)
+				if err != nil {
+					return "", err
 				}
-			case <- time.After(1 * time.Second) :
-				tftp.SendACK(prev_block, Conn)
+				if block[1] == prev_block[1]+1 {
+					tftp.SendACK(block, Conn)
+					prev_block[1] = block[1]
+					temp = append(temp, data...)
+					if len(data) < 512 {
+						break Read
+					}
+				} else if block[1] != 0 {
+					tftp.SendACK(prev_block, Conn)
+				} // else the RRQ itself failed somehow
 
-			case <- time.After(15 * time.Second) :
-				fmt.Println("A read request by ", LocalAddr, " timed out")
-				return "", errors.New("rrq timed out")
+			case 5:
+				//ERROR packet recieved
+				_, errMsg, err := tftp.GetError(r)
+				if err != nil {
+					return "", err
+				}
+				fmt.Println(string(errMsg), " recieved by ", LocalAddr)
+				return "", errors.New(string(errMsg))
+			}
+		case <-time.After(1 * time.Second):
+			tftp.SendACK(prev_block, Conn)
+
+		case <-time.After(15 * time.Second):
+			fmt.Println("A read request by ", LocalAddr, " timed out")
+			return "", errors.New("rrq timed out")
 		}
 	}
 
@@ -215,37 +226,41 @@ func sendFullRead(filename string) (string, error){
 /* BEGIN TESTS */
 
 func TestOneWRQ(*testing.T) {
-		sendFullWrite("TestOneWRQ", "TestOneWRQ " + longFile)
-} // */ 
+	sendFullWrite("TestOneWRQ", "TestOneWRQ "+longFile)
+} // */
 
-func TestManyWRQ(*testing.T){
-	for i:=0; i < 5; i++ {
-		go sendFullWrite("TestManyWRQ" + strconv.Itoa(i), "Hello, World " + strconv.Itoa(i) + "! " + longFile)
+func TestManyWRQ(*testing.T) {
+	for i := 0; i < 5; i++ {
+		go sendFullWrite("TestManyWRQ"+strconv.Itoa(i), "Hello, World "+strconv.Itoa(i)+"! "+longFile)
 	}
 } // */
 
 func TestOneRRQ(t *testing.T) {
 	temp := "Test a read! " + longFile
-	sendFullWrite("TestOneRRQ", temp )
-	read,err := sendFullRead("TestOneRRQ")
-	if(err != nil) { return }
-	if(len(temp) != len(read)){ 
-		t.FailNow() 
+	sendFullWrite("TestOneRRQ", temp)
+	read, err := sendFullRead("TestOneRRQ")
+	if err != nil {
+		return
+	}
+	if len(temp) != len(read) {
+		t.FailNow()
 	} else {
 		for i := 0; i < len(temp); i++ {
-			if( temp[i] != read[i] ) { t.FailNow() }
+			if temp[i] != read[i] {
+				t.FailNow()
+			}
 		}
 	}
 } // */
 
 func TestManyRRQ(*testing.T) {
-	sendFullWrite("TestManyRRQ", "Testing many reads! " + longFile)
-	for i:= 0; i < 5; i++ {
+	sendFullWrite("TestManyRRQ", "Testing many reads! "+longFile)
+	for i := 0; i < 5; i++ {
 		go sendFullRead("TestManyRRQ")
 	}
 } // */
 
 func TestOneOverWrite(*testing.T) {
-	sendFullWrite("TestOneOverWrite", "TestOneOverWrite " + longFile)
-	sendFullWrite("TestOneOverWrite", "TestOneOverWrite, overwritten " + longFile)
+	sendFullWrite("TestOneOverWrite", "TestOneOverWrite "+longFile)
+	sendFullWrite("TestOneOverWrite", "TestOneOverWrite, overwritten "+longFile)
 } // */
